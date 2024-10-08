a8d2fab82ce03164301f664b65f1e6d58ad65d72
cybersobic <cybersobic@gmail.com>
2024-10-08 15:35:25 +0300

    Initialize project

---
+++ src/main/java/com/zspps/store/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.zspps.store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController
+{
+    @GetMapping("/")
+    public String home(Model model)
+    {
+        model.addAttribute("title", "Интернет-магазин Zenith");
+        return "home";
+    }
+
+}
